30c7c067973c34ce9f98d981b60d2c12c4108195
AlexSkilled <olegthander@yandex.ru>
2026-03-17 15:31:41 +0300

    [master] missing migration

---
+++ migrations/20260317143500_user_statistics.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE user_statistics (
+    username TEXT PRIMARY KEY,
+    last_connect DATETIME DEFAULT CURRENT_TIMESTAMP,
+    bytes_passed BIGINT DEFAULT 0,
+    FOREIGN KEY(username) REFERENCES users(username) ON DELETE CASCADE
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE user_statistics;
+-- +goose StatementEnd
