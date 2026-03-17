a19168168ddc771d0c975d689b034ed9855ae75d
AlexSkilled <olegthander@yandex.ru>
2026-03-17 16:19:41 +0300

    [master] dummy stats

--- internal/server/server.go
+++ internal/server/server.go
@@ -465,22 +465,19 @@ func dialErrToReply(err error) uint8 {
 
 // relay copies data bidirectionally between a and b until either side closes.
 func (s *Server) relay(a, b net.Conn, username, remote, target string) {
-	var bytesPassed atomic.Int64
-
 	var wg sync.WaitGroup
 	wg.Add(2)
+
+	var bytesPassed atomic.Int64
 	copy := func(dst, src net.Conn, dir string) {
 		defer wg.Done()
-		if tcpConn, ok := dst.(*net.TCPConn); ok {
-			tcpConn.CloseWrite() //nolint:errcheck
-		}
+		defer dst.(*net.TCPConn).CloseWrite() //nolint:errcheck
 
 		buf := make([]byte, 32*1024)
 		for {
 			n, err := src.Read(buf)
 			if n > 0 {
 				bytesPassed.Add(int64(n))
-
 				log.Info().
 					Str(log_key.Username, username).
 					Str(log_key.RemoteAddr, remote).
